812e5705dbffe5900eedaa171eed2070420f2efa
Milad <khodaverdi.milad23@gmail.com>
2022-03-15 16:32:49 +0330

    feat: add coin ajax

---
+++ app/Http/Controllers/CoinController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Coin;
+use Illuminate\Http\JsonResponse;
+use Yajra\DataTables\Facades\DataTables;
+
+class CoinController extends Controller
+{
+    public function ajax(): JsonResponse
+    {
+        $coin = Coin::query();
+        return DataTables::eloquent($coin)->toJson();
+    }
+}
